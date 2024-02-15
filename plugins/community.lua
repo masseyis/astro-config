0b9dee9053315bb72ef918e592a514bf84add513
james-tru <james@trufintech.io>
2024-02-15 23:21:49 +0000

    Added noice and easyalign. Disabl;ed tabnine and enabled copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,13 +6,16 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.completion.tabnine-nvim"},
+  -- { import = "astrocommunity.completion.tabnine-nvim"},
   -- { import = "astrocommunity.bars-and-lines.dropbar-nvim"},
   { import = "astrocommunity.editing-support.comment-box-nvim"},
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
   { import = "astrocommunity.editing-support.vim-move"},
   { import = "astrocommunity.editing-support.wildfire-nvim"},
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.terraform"},
   { import = "astrocommunity.pack.yaml"},
+  { import = "astrocommunity.utility.noice-nvim"},
+  { import = "astrocommunity.motion.nvim-surround"},
+  { import = "astrocommunity.syntax.vim-easy-align"},
 }
